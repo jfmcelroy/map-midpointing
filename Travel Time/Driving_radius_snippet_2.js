08bafa68c62ab5dddbe1ca552128a4d8ebf22bc8
James McElroy <105364772+jfmcelroy@users.noreply.github.com>
2022-07-27 13:18:19 -0400

    Create Driving_radius_snippet_2.js

---
+++ Travel Time/Driving_radius_snippet_2.js
@@ -0,0 +1,5 @@
+var startingLocation = "London Waterloo Train Station";
+var departureTime =  new Date().toJSON();
+var travelTime = 60 * 15;
+var APPLICATION_ID = "place your app id here";
+var API_KEY = "place your api key here";
